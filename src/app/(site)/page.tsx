897d7eeab81125e2e6c1af07014b6d36487e9d9c
sushanthach12 <sushacharya7758@gmail.com>
2024-06-06 19:20:28 +0530

    Image

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -5,11 +5,12 @@ const Home = async () => {
 
     return (
         <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md h-10 w-10">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <Image
-                    src={"/images/placeholder.jpg"}
+                    src={"/images/logo.png"}
+                    height={48}
+                    width={48}
                     alt="Logo"
-                    placeholder="blur"
                     className="mx-auto w-auto"
                 />
 
